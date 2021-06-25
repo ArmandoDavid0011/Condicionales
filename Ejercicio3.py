0b0fa8f5712b3aded8c57c627656dfcb9ee46d9c
Armando David Camacho Merino <83479028+ArmandoDavid0011@users.noreply.github.com>
2021-06-25 01:14:16 -0500

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,14 +1,5 @@
 # Ejercicio 3 if
-"""
-nombre1=input("nombre 1: ")
-nombre2=input("nombre 2: ")
-if nombre1==nombre2:
-    print("Son Similares")
-    print("Sigue asi bb tu puedes mi amor")
-elif nombre1!=nombre2:
-    print("Son pendejos xd")
-    print("Como eres pendejo ni escribir nombre sabes")
-"""
+
 nombre1=input("Nombre 1: ")
 nombre2=input("Nombre 2: ")
 if nombre1[0] == nombre2[0] or nombre1[-1] == nombre2[-1]:
